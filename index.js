f4e008a63eb5a87049277fb0e05e2d9a8cfdf7fe
tlcraft <tlcraft@users.noreply.github.com>
2018-08-31 17:19:56 -0600

    Adding initial kana set and refactoring component logic

--- index.js
+++ index.js
@@ -3,26 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function KanaBoard (props) {
+    let kana = props.kanaSet.map((k, i) => { 
+        return (
+          <Kana key={i}
+            hira={k.hira}
+            kata={k.kata}
+            pronunciation={k.eng}
+          />
+        );
+      });
+
     return (
         <div>
             <h1>Hello World!</h1>
             <p>{props.message}</p>
-            <KanaRow />
+            {kana}
         </div>
     );
 }
 
-function KanaRow (props) {
-    return (
-        <Kana character="つ" pronunciation="tsu"/>
-    )
-}
+const KANA = [
+    {hira: 'あ', kata: 'ア', eng: 'a'},
+    {hira: 'い', kata: 'イ', eng: 'i'},
+    {hira: 'う', kata: 'ウ', eng: 'u'},
+    {hira: 'え', kata: 'エ', eng: 'e'},
+    {hira: 'お', kata: 'オ', eng: 'o'},
+
+    {hira: 'た', kata: 'タ', eng: 'ta'},
+    {hira: 'ち', kata: 'チ', eng: 'chi'},
+    {hira: 'つ', kata: 'ツ', eng: 'tsu'},
+    {hira: 'て', kata: 'テ', eng: 'te'},
+    {hira: 'と', kata: 'ト', eng: 'to'},
+  ];
 
 function Kana (props) {
     return (
         <div>
             <div>
-                {props.character}
+                {props.hira}
+            </div>
+            <div>
+                {props.kata}
             </div>
             <div>
                 {props.pronunciation}
@@ -32,6 +53,43 @@ function Kana (props) {
 }
 
 ReactDOM.render(
-    <KanaBoard message="How are you? We're just getting started!" />,
+    <KanaBoard message="How are you? We're just getting started!" kanaSet={KANA} />,
     document.getElementById('root')
   );
+ 
+  
+  /*
+
+  Partial set of：
+
+  Katakana
+
+  カキクケコ
+  ガギグゲゴ
+  サシスセソ
+  ザジズゼゾ
+  ラリルレロ
+  ハヒフヘホ
+  バビブベボ
+  パピプペポ
+  ナニヌネノ
+  マミムメモ
+  ヤユヨ
+  ンヲ  
+  
+  Hiragana
+
+  かきくけこ
+  がぎぐげご
+  さしすせそ
+  ざじずぜぞ
+  らりるれろ
+  はひふへほ
+  ばびぶべぼ
+  ぱぴぷぺぽ
+  なにぬねの
+  まみむめも
+  やゆよ
+  んを
+  
+  */
