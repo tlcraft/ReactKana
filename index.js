fffd3a70531f430e1292ec6b9d1580079c5f97bb
tlcraft <tlcraft@users.noreply.github.com>
2018-09-01 12:31:20 -0600

    Further defined kana list, added basic styling to kana

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function KanaBoard (props) {
             hira={k.hira}
             kata={k.kata}
             pronunciation={k.eng}
+            className="kana"
           />
         );
       });
 
     return (
         <div>
-            <h1>Hello World!</h1>
-            <p>{props.message}</p>
+            <h1>{props.message}</h1>
             {kana}
         </div>
     );
@@ -29,23 +29,41 @@ const KANA = [
     {hira: 'え', kata: 'エ', eng: 'e'},
     {hira: 'お', kata: 'オ', eng: 'o'},
 
+    {hira: 'か', kata: 'カ', eng: 'ka'},
+    {hira: 'き', kata: 'キ', eng: 'ki'},
+    {hira: 'く', kata: 'ク', eng: 'ku'},
+    {hira: 'け', kata: 'ケ', eng: 'ke'},
+    {hira: 'こ', kata: 'コ', eng: 'ko'},
+    
+    {hira: 'さ', kata: 'サ', eng: 'sa'},
+    {hira: 'し', kata: 'シ', eng: 'shi'},
+    {hira: 'す', kata: 'ス', eng: 'su'},
+    {hira: 'せ', kata: 'セ', eng: 'se'},
+    {hira: 'そ', kata: 'ソ', eng: 'so'},
+
     {hira: 'た', kata: 'タ', eng: 'ta'},
     {hira: 'ち', kata: 'チ', eng: 'chi'},
     {hira: 'つ', kata: 'ツ', eng: 'tsu'},
     {hira: 'て', kata: 'テ', eng: 'te'},
     {hira: 'と', kata: 'ト', eng: 'to'},
+
+    //TODO Complete array
   ];
+
+// function KanaRow(props) {
+//     // TODO Group kana together
+// }
 
 function Kana (props) {
     return (
-        <div>
-            <div>
+        <div className={props.className}>
+            <div className='hira'>
                 {props.hira}
             </div>
-            <div>
+            <div className='kata'>
                 {props.kata}
             </div>
-            <div>
+            <div className='pronunciation'>
                 {props.pronunciation}
             </div>
         </div>
@@ -53,43 +71,77 @@ function Kana (props) {
 }
 
 ReactDOM.render(
-    <KanaBoard message="How are you? We're just getting started!" kanaSet={KANA} />,
+    <KanaBoard message="Japanese Hiragana and Katakana Syllabaries" kanaSet={KANA} />,
     document.getElementById('root')
   );
  
   
   /*
 
-  Partial set of：
-
-  Katakana
-
-  カキクケコ
-  ガギグゲゴ
-  サシスセソ
-  ザジズゼゾ
-  ラリルレロ
-  ハヒフヘホ
-  バビブベボ
-  パピプペポ
-  ナニヌネノ
-  マミムメモ
-  ヤユヨ
-  ンヲ  
-  
   Hiragana
-
-  かきくけこ
-  がぎぐげご
-  さしすせそ
-  ざじずぜぞ
-  らりるれろ
-  はひふへほ
-  ばびぶべぼ
-  ぱぴぷぺぽ
+  
+  あいうえお
+  かきくけこ  
+  さしすせそ  
+  たちつてと  
   なにぬねの
+  はひふへほ  
   まみむめも
   やゆよ
-  んを
+  らりるれろ
+  わを
+  ん
+
+　がぎぐげご
+　ざじずぜぞ
+　だぢづでど
+　ばびぶべぼ
+  ぱぴぷぺぽ
+
+  きゃきゅきょ
+  しゃしゅしょ
+  ちゃちゅちょ
+  にゃにゅにょ
+  ひゃひゅひょ
+  みゃみゅみょ
+  りゃりゅりょ
+  ぎゃぎゅぎょ
+  じゃじゅじょ
+  ぢゃぢゅぢょ
+  びゃびゅびょ
+  ぴゃぴゅぴょ
   
+　Katakana
+
+  アイウエオ
+  カキクケコ  
+  サシスセソ  
+  タチツテト  
+  ナニヌネノ
+  ハヒフヘホ 
+  マミムメモ
+  ヤユヨ
+  ラリルレロ
+  ワヲ
+  ン  
+  
+　ガギグゲゴ
+　ザジズゼゾ
+  ダヂヅデド
+　バビブベボ
+  パピプペポ
+
+　キャキュキョ
+　シャシュショ
+　チャチュチョ
+　ニャニュニョ
+　ヒャヒュヒョ
+　ミャミュミョ
+　リャリュリョ
+　ギャギュギョ
+　ジャジュジョ
+　ヂャヂュヂョ
+　ビャビュビョ
+　ピャピュピョ
+
   */
